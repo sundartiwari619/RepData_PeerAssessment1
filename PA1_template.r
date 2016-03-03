947dc532ecf9e856c1cdee09bb61f97c1f479143
Sundar Tiwari <sundartiwari619@gmail.com>
2016-03-03 21:10:29 +0530

    Corrected code

--- PA1_template.r
+++ PA1_template.r
@@ -1,57 +1,50 @@
-# Reproducible Research: Peer Assessment 1
 
-## Loading and preprocessing the data
-```{r loaddata}
-unzip your file "activity.zip"
-Load the file, read the data using read.csv()
-activity <- read.csv("activity.csv")
-```
+## This first line will likely take a few seconds. Be patient!
+if(!exists("activity")){
+  activity <- read.csv("activity.csv")
+}
 
 ## What is mean total number of steps taken per day?
-```{r}
 total <- aggregate(steps ~ date, activity, sum, na.rm=TRUE)
-mean(total, na.rm=TRUE)
-median(total, na.rm=TRUE)
-```
+png('figure1.png')
+barplot(height=total$steps, names.arg=total$date, xlab="dates", ylab=expression('total steps'),main=expression('Total steps per day'))
+dev.off()
+mean(total$steps)
+median(total$steps)
+
 
 ## What is the average daily activity pattern?
-```{r}
+library(ggplot2)
 intervalTotal <- aggregate(steps ~ interval, activity, mean, na.rm=TRUE)
+png('figure2.png')
 plot(intervalTotal$interval,intervalTotal$steps,type="l", xlab="Interval", ylab= "steps", main = "Steps by Interval")
-```
-What is the maximum number of steps by interval
-```{r}
+dev.off()
 
 intervalTotal[which.max(intervalTotal$steps),]
-```
-
 
 ## Imputing missing values
-```{r}
 missingValues <- is.na(activity$steps)
 table(missingValues)
-```
-Replacing NA setp values with mean values
-```{r}
+
 library(Hmisc)
 
 completeActivity <- activity
 completeActivity$steps <- impute(completeActivity$steps,fun=mean)
 totalimputed <- aggregate(steps ~ interval, completeActivity, sum)
+png('figure3.png')
 plot(totalimputed$interval,totalimputed$steps,type="h", xlab="Interval", ylab= "steps", main = "Imputed Steps by Interval")
+dev.off()
 
 mean(totalimputed$steps)
 median(totalimputed$steps)
-```
 
-## Are there differences in activity patterns between weekdays and weekends?
-```{r}
-library(ggplot2)
+
+##Are there differences in activity patterns between weekdays and weekends?
 
 completeActivity$dayType <-  ifelse(as.POSIXlt(completeActivity$date)$wday %in% c(0,6), 'weekend', 'weekday')
 
 totalDayType <- aggregate(steps ~ interval+dayType, completeActivity, mean)
-
+png('figure4.png')
 ggplot(totalDayType, aes(interval, steps)) + geom_line() + facet_grid(dayType ~ .) +
   xlab("5-minute interval") + ylab("Number of steps")
-```
+dev.off()
